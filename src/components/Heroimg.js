a78d9537eaa155607611a244fd87976e41d8d109
Sreyas <115882857+sreyas-sc@users.noreply.github.com>
2022-11-07 22:17:37 +0530

    Update Heroimg.js

--- src/components/Heroimg.js
+++ src/components/Heroimg.js
@@ -1,6 +1,6 @@
 import "./HeroimgStyles.css";
 import React from 'react';
-import IntroImg from "../Assets/img5.jpg"
+import IntroImg from "../Assets/img3.jpg"
 import { Link } from "react-router-dom";
 
 const Heroimg = () => {
