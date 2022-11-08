f7d03da4af43214811184b7e29125976eddaa6fa
Sreyas <115882857+sreyas-sc@users.noreply.github.com>
2022-11-08 14:22:18 +0530

    Update AboutContent.js

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -7,8 +7,8 @@ const AboutContent = () => {
     <div className="about">
         <div className="left">
             <h1>Know more about me</h1>
-            <p>Hi! I'm a student, Developer currenlty persuing my Masters in Computer Applications at CET<br>
-    2021 - 2022 :Software Analyst(L3) at Claysys Technologies</p>
+            <p>Hi! I'm a student, Developer currenlty persuing my Masters in Computer Applications at CET</p>
+            <p>2021 - 2022 Software Analyst(L3) at Claysys Technologies</p>
             <Link to="/Contact">
                 <button className="btn">Contact</button>
             </Link>
