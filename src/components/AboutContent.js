91d03b112d91cd6b97242a8363e5f4069af7039f
Sreyas <115882857+sreyas-sc@users.noreply.github.com>
2022-11-08 14:32:03 +0530

    Update AboutContent.js

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -10,7 +10,6 @@ const AboutContent = () => {
             <p>Hi! I'm a student, Developer currenlty persuing my Masters</p>
             <p>2022 - Present: Pursuing MCA at <a href="https://www.cet.ac.in/"><u>CET</u></a></p>
             <p>2021 - 2022: Software Analyst(L3) at <a href="https://www.claysys.com/"><u>Claysys Technologies</u></a></p>
-            <p>2018 - 2021: Studied BCA at <a href="https://www.naipunnyacollege.ac.in/">Naipunnya School of Management Cherthala</a></p>
             <Link to="/Contact">
                 <button className="btn">Contact</button>
             </Link>
