41e2a0c8068c9087e5487df527da8f2caa2ce385
Sreyas <115882857+sreyas-sc@users.noreply.github.com>
2023-11-29 11:14:26 +0530

    Update HeroImg.js

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -10,7 +10,39 @@ const Heroimg = () => {
             <img className="into-img" src= {IntroImg} alt="IntroImg"/>
         </div>
         <div className="content">
-            <p>Hi I'm Sreyas S, This is my personal website</p>
+            <p>Hi I'm Sreyas S, This is my personal website
+
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button=findViewById(R.id.btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.google.com"));
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
+
+</p>
             <h1> </h1>  
             <div>
                 <Link to="/project" className="btn">Projects</Link>
