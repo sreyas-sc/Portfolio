06ab51879a02f7e147cafa9d926df1de0068d7ec
Sreyas <115882857+sreyas-sc@users.noreply.github.com>
2024-06-26 22:15:52 +0530

    Update HeroImg.js

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -1,55 +1,49 @@
-import "./HeroImgStyles.css"
-import React from 'react'
+import React from 'react';
+import "./HeroImgStyles.css";
+import IntroImg from "../assets/back1.jpg";
+import MyImg from "../assets/myimage.png"; // Assuming you have imported your image
 
-import IntroImg from "../assets/back1.jpg"
-import { Link } from "react-router-dom"
 const Heroimg = () => {
   return (
     <div className="hero">
         <div className="mask">
-            <img className="into-img" src= {IntroImg} alt="IntroImg"/>
+            {/* <img className="into-img" src={IntroImg} alt="IntroImg"/> */}
         </div>
+
         <div className="content">
-            <p>Hi I'm Sreyas S, This is my personal website
-
-package com.example.myapplication;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button button=findViewById(R.id.btn);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(Intent.ACTION_VIEW);
-                intent.setData(Uri.parse("https://www.google.com"));
-                startActivity(intent);
-            }
-        });
-
-
-    }
-}
-
-</p>
-            <h1> </h1>  
+            <p className="hero .content p">Hi I'm Sreyas S</p>
+            <h1></h1>
             <div>
-                <Link to="/project" className="btn">Projects</Link>
+                {/* <button Link to="/project">Projects</button> */}
+                {/* <Link to="/project" className="btn">Projects</Link> */}
             </div>
+            {/* <div class="dropcard"></div> */}
         </div>
+        
+
+        <div className="profile-image">
+            <img src={MyImg} alt="Profile Image" className="profile-img" />
+        </div>
+
+        <div className="aboutcard">
+            <div className="cardcontent">
+                <p className="cardheading">ABOUT ME</p>
+                <p className="cardpara">
+                    I am a MCA graduate with a passion towards coding and web development residing in Alappuzha of kerala with 1year + experience in the IT field
+                </p>
+                <button className="btn">My Resume</button>
+            </div>
+            
+        </div>
+        {/* <div className='resume'>
+        <button>
+  <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"></polygon></svg> Explore me
+</button>
+        </div> */}
+
     </div>
-  )
+    
+  );
 }
 
-export default Heroimg
+export default Heroimg;
