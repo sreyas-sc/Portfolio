3a279df4dfb816f4ade165c1e437259aab038177
Sreyas <115882857+sreyas-sc@users.noreply.github.com>
2024-06-28 20:19:55 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -39,52 +39,41 @@ const Footer  = () => {
             </div>
 
              <div className="right">
-                <h4>About Me</h4>
+                {/* <h4>About Me</h4>
                 <p>Hi! This is Sreyas.
                  
-                 </p>
-                {/* <div className="social">
-                 <FaFacebook size={30} style={{color:"#fff", marginRight:"1rem"}}/>
-                 <FaTwitter size={30} style={{color:"#fff", marginRight:"1rem"}}/>
-                 <FaLinkedin size={30} style={{color:"#fff", marginRight:"1rem"}}/>
-                 <FaInstagramSquare size={30} style={{color:"#fff", marginRight:"1rem"}}/>
-                 <FaWhatsapp size={30} style={{color:"#fff", marginRight:"1rem"}}/>
-                 <FaEnvelope size={30} style={{color:"#fff", marginRight:"1rem"}}/>
-                 <FaGithub size={30} style={{color:"#fff", marginRight:"1rem"}}/>
-                 <FaTelegram size={30} style={{color:"#fff", marginRight:"1rem"}} />             
-                 
-                 </div> */}
+                 </p> */}
 
                  <div class="social-container">
                     <h3> </h3>
-                    <a href="https://www.facebook.com/sree.sreyass"
+                    {/* <a href="https://www.facebook.com/sree.sreyass"
         className="facebook social">
         <FontAwesomeIcon icon={faFacebook} size="2x" style={{marginRight:'1rem'}}/>
-      </a>
-      <a href='https://www.instagram.com/sreyas.s_/'
+      </a> */}
+      {/* <a href='https://www.instagram.com/sreyas.s_/'
         className="instagram social">
         <FontAwesomeIcon icon={faInstagram} size="2x" style={{marginRight:'1rem'}}/>
-      </a>
-      <a href="https://twitter.com/SREYAS_S_" className="twitter social">
+      </a> */}
+      {/* <a href="https://twitter.com/SREYAS_S_" className="twitter social">
         <FontAwesomeIcon icon={faTwitter} size="2x" style={{marginRight:'1rem'}}/>
-      </a>
-      <a href="https://github.com/sreyas-sc"
+      </a> */}
+      {/* <a href="https://github.com/sreyas-sc"
         className="github social">
         <FontAwesomeIcon icon={faGithub} size="2x" style={{marginRight:'1rem'}}/>
-      </a>
+      </a> */}
       
-      <a href="https://wa.me/918111904512?Hey!"
+      {/* <a href="https://wa.me/918111904512?Hey!"
         className="whatsapp social">
         <FontAwesomeIcon icon={faWhatsapp} size="2x" style={{marginRight:'1rem'}}/>
-      </a>
-      <a href="https://www.linkedin.com/in/sreyas-s-27857a205"
+      </a> */}
+      {/* <a href="https://www.linkedin.com/in/sreyas-s-27857a205"
         className="linkedin social">
         <FontAwesomeIcon icon={faLinkedin} size="2x" style={{marginRight:'1rem'}}/>
-      </a>
-      <a href="href='t.me/@sreyas_s"
+      </a> */}
+      {/* <a href="href='t.me/@sreyas_s"
         className="telegram social">
         <FontAwesomeIcon icon={faTelegram} size="2x" style={{marginRight:'1rem'}}/>
-      </a>
+      </a> */}
 </div>
             </div> 
         </div>
