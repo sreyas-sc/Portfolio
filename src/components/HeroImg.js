daf2d534479661b1a2d643ea3545bee258ba420e
Sreyas <115882857+sreyas-sc@users.noreply.github.com>
2024-07-06 13:33:14 +0530

    Update HeroImg.js

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -635,21 +635,12 @@ const Heroimg = () => {
   </li>
   
 </ul>
-
-
-
 </div>
-
-
         {/* <div className='resume'>
         <button>
   <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"></polygon></svg> Explore me
 </button>
         </div> */}
-
-
-        
-
     </div>
 
     
